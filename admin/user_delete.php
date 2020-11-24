064f1daf7ce65129196b3ebf091deb85916237cf
Si Thu Tin <sethu.tin@gmail.com>
2020-11-25 00:14:14 +0630

    new

--- admin/user_delete.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-require '../config/config.php';
-$stmt = $pdo->prepare("DELETE FROM users WHERE id=".$_GET['id']);
-$stmt->execute();
-
-header('Location: user_list.php');
-?>
--- cart_item_clear.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-  session_start();
-
-  unset($_SESSION['cart']['id'.$_GET['pid']]);
-
-  header("Location: cart.php");
-?>
